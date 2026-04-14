a75350f7b03b23c0c6dc8e040bf4ef1b550b4998
adrixnx1 <abarcenas2020@gmail.com>
2026-04-14 10:10:01 -0500

    added timeout + settimeout + log + readfile + made the window circular queue + made the fill window + set up the packet and almost finished up the waiting for ack

--- PA3/tx.py
+++ PA3/tx.py
@@ -7,6 +7,7 @@ from pa3.cQueue import CircularQueue
 PAYLOAD_SIZE = 50
 SEQNUM_SIZE = 10
 WINDOW_SIZE = 5
+TIMEOUT = 0.5
 
 
 def reliablyTransfer(rx_ip, rx_port, filename):
@@ -14,6 +15,51 @@ def reliablyTransfer(rx_ip, rx_port, filename):
     # Create log files as well to log the events in the sender side
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.connect((rx_ip, rx_port))
+
+    #adriana - set timeoout for the socket + added logs
+    sock.settimeout(TIMEOUT)
+    tx_seq_log = open("TxSeqNum.log", "w")
+    tx_ack_log = open("TxAck.log", "w")
+
+    #next i am doing the read file
+    with open(filename, "rb") as f:
+        file_data = f.read()
+
+    #window using circular queue
+    window = CircularQueue(WINDOW_SIZE)
+
+    next_seqnum = 0
+    file_index = 0
+
+    while True:
+
+        #fill window (PIPELINE SENDING)
+        while not window.isFull() and file_index < len(file_data):
+           
+           chunk = file_data[file_index:file_index+PAYLOAD_SIZE]
+
+           pkt = Packet(
+               flag=1, # data packet
+               seqnum=next_seqnum,
+                length=len(chunk),
+                payload=chunk.decode("utf-8") #dear agafia, im using this to convert bytes to string, if it doesnt work please lmk
+           )
+
+          window.enqueue(pkt)
+
+            #set up the sending packet
+              sock.send(pkt.serialize())
+        tx_seq_log.write(f"{pkt.seqnum}\n")
+
+        file_index +=len(chunk)
+        next_seqnum = (next_seqnum + 1) % SEQNUM_SIZE
+
+        #waiting for ack
+
+        try:
+            data
+
+
     sock.send(b"hi!")
     # TODO: later, loop until FINACK is received (I think)
     msg_recv = sock.recv(50)
